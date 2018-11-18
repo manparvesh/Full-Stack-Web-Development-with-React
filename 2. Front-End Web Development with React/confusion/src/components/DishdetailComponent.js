7e108f4bb7cf21a469f2ebf3f77b04e7e348c87b
Man Parvesh Singh Randhawa <manparveshsinghrandhawa@gmail.com>
2018-11-18 21:34:19 +0800

    Presentational and Container Components

--- 2. Front-End Web Development with React/confusion/src/components/DishdetailComponent.js
+++ 2. Front-End Web Development with React/confusion/src/components/DishdetailComponent.js
@@ -6,7 +6,7 @@ class Dishdetail extends Component {
   renderDish(dish) {
     return (
       <Card>
-        <CardImg width="100%" src={dish.image} alt={dish.name}/>
+        <CardImg width="100%" src={dish.image} alt={dish.name} />
         <CardBody>
           <CardTitle>{dish.name}</CardTitle>
           <CardText>{dish.description}</CardText>
@@ -19,10 +19,15 @@ class Dishdetail extends Component {
     if (comments != null) {
       const commentList = comments.map(comment => {
         return (
-          <li>
+          <li key={comment.id}>
             <p>{comment.comment}</p>
             <p>
-              -- {comment.author}, {comment.date}
+              -- {comment.author},{" "}
+              {new Intl.DateTimeFormat("en-US", {
+                year: "numeric",
+                month: "short",
+                day: "2-digit"
+              }).format(new Date(Date.parse(comment.date)))}
             </p>
           </li>
         );
@@ -42,12 +47,14 @@ class Dishdetail extends Component {
   render() {
     if (this.props.dish != null) {
       return (
-        <div className="row">
-          <div className="col-12 col-md-5 m-1">
-            {this.renderDish(this.props.dish)}
-          </div>
-          <div className="col-12 col-md-5 m-1">
-            {this.renderComments(this.props.dish.comments)}
+        <div className="container">
+          <div className="row">
+            <div className="col-12 col-md-5 m-1">
+              {this.renderDish(this.props.dish)}
+            </div>
+            <div className="col-12 col-md-5 m-1">
+              {this.renderComments(this.props.dish.comments)}
+            </div>
           </div>
         </div>
       );
