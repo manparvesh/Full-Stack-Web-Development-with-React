3a938f7c7b38a45bc97f56f36bf02633c00ef571
Man Parvesh Singh Randhawa <manparveshsinghrandhawa@gmail.com>
2018-11-18 22:19:05 +0800

    React Router

--- 2. Front-End Web Development with React/confusion/src/components/MainComponent.js
+++ 2. Front-End Web Development with React/confusion/src/components/MainComponent.js
@@ -2,8 +2,10 @@ import React, { Component } from "react";
 import Menu from "./MenuComponent";
 import DishDetail from "./DishdetailComponent";
 import { DISHES } from "../shared/dishes";
-import Header from './HeaderComponent';
-import Footer from './FooterComponent';
+import Header from "./HeaderComponent";
+import Footer from "./FooterComponent";
+import Home from "./HomeComponent";
+import { Switch, Route, Redirect } from "react-router-dom";
 
 class Main extends Component {
   constructor(props) {
@@ -19,9 +21,22 @@ class Main extends Component {
   }
 
   render() {
+    const HomePage = () => {
+      return <Home />;
+    };
+
     return (
       <div>
         <Header />
+        <Switch>
+          <Route path="/home" component={HomePage} />
+          <Route
+            exact
+            path="/menu"
+            component={() => <Menu dishes={this.state.dishes} />}
+          />
+          <Redirect to="/home" />
+        </Switch>
         <Menu
           dishes={this.state.dishes}
           onClick={dishId => this.onDishSelect(dishId)}
